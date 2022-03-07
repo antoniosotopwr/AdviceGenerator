98b6f28b423a38cab7716771e0877a4c4b3b4b12
Antonio Soto <antoniosotopwr@gmail.com>
2022-03-07 11:46:01 -0600

    Advice generator api v1

---
+++ api.js
@@ -0,0 +1,12 @@
+const api_url = "https://api.adviceslip.com/advice";
+
+async function getapi(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  var { id, advice } = data.slip;
+
+  document.getElementById("advice").textContent = '"' + advice + '"'; 
+  document.getElementById("adviceNumber").textContent =  id ;
+}
+
+getapi(api_url);
